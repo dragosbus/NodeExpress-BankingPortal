c33afce88541d06c98e079bc6ffd345455eb1aed
dragosbus <dragosbusuioc94@gmail.com>
2018-09-25 16:33:50 +0200

    Completed the module

--- src/app.js
+++ src/app.js
@@ -7,10 +7,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req,res)=>{
-  res.render('index', {title:'Index'})
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Account Summary',
+    accounts: accounts
+  })
 });
 
-app.listen(3000, ()=>{
+app.get('/savings', (req,res)=>{
+  res.render('account', {account:accounts.savings})
+});
+
+app.get('/checking', (req,res)=>{
+  res.render('account', {account:accounts.checking})
+});
+
+app.get('/credit', (req,res)=>{
+  res.render('account', {account:accounts.credit})
+});
+
+app.get('/profile', (req, res)=>{
+  res.render('profile', {user: users[0]})
+});
+
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), 'utf-8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), 'utf-8');
+const users = JSON.parse(userData);
+
+app.listen(3000, () => {
   console.log('Listen');
 });
